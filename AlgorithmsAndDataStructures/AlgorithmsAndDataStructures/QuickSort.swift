5a177f88b5ac2f3d3453d3d83c0d9b8441386827
Gergely Orosz <gergely.orosz@gmail.com>
2014-08-09 11:59:13 +0100

    Quicksort

---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructures/QuickSort.swift
@@ -0,0 +1,70 @@
+//
+//  InsertionSort.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Carolyn Johnson on 30/07/2014.
+//  Copyright (c) 2014 com.gergelyorosz. All rights reserved.
+//
+
+import Foundation
+
+class QuickSort
+{
+    class func sort(array: Int[]) {
+        quicksort(array, left:0, right:array.count-1)
+    }
+    
+    class func quicksort(array: Int[], left: Int, right: Int) {
+        if left < right {
+            var partitionIndex = partition(array, left: left, right: right)
+            quicksort(array, left:left, right: partitionIndex-1)
+            quicksort(array, left:partitionIndex+1, right: right)
+        }
+    }
+    
+    class func partition(array: Int[], left: Int, right: Int) -> Int {
+        var pivotIndex = choosePivotIndex(array, left: left, right: right);
+        var pivotValue = array[pivotIndex]
+        var storeIndex = left
+        swap(array, index1: pivotIndex, index2: right)
+        for (var i=left; i<right; i++) {
+            if array[i] <= pivotValue {
+                swap(array, index1: i, index2: storeIndex)
+                storeIndex += 1
+            }
+        }
+        swap(array, index1: storeIndex, index2: right)
+        return storeIndex
+    }
+    
+    class func swap(array: Int[], index1: Int, index2: Int) {
+        var tmp = array[index1];
+        array[index1] = array[index2]
+        array[index2] = tmp
+    }
+    
+    class func choosePivotIndex(array: Int[], left: Int, right: Int) -> Int {
+        var middle = (left + right) / 2
+        if array[left] <= array[middle] {
+            if array[middle] <= array[right] {
+                return middle
+            }
+            else if array[left] >= array[right] {
+                return left
+            }
+            else {
+                return right
+            }
+        } else { // array[left] >= array[middle]
+            if array[left] <= array[right] {
+                return left
+            }
+            else if array[middle] >= array[right] {
+                return middle
+            }
+            else {
+                return right
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/AlgorithmsAndDataStructuresTests/QuickSortTests.swift
@@ -0,0 +1,46 @@
+//
+//  InsertionSortTests.swift
+//  AlgorithmsAndDataStructures
+//
+//  Created by Carolyn Johnson on 30/07/2014.
+//  Copyright (c) 2014 com.gergelyorosz. All rights reserved.
+//
+
+import XCTest
+import AlgorithmsAndDataStructures
+
+class QuickSortTests: XCTestCase {
+    
+    func testSortingAnEmptyArray() {
+        SortingTests.testSortingAnEmptyArray(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithOneItem() {
+        SortingTests.testSortingArrayWithOneItem(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithTwoItemsInOrder() {
+        SortingTests.testSortingArrayWithTwoItemsInOrder(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithTwoItemsInReversedOrder() {
+        SortingTests.testSortingArrayWithTwoItemsInReversedOrder(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithMultipleItemsSortedExceptForOne() {
+        SortingTests.testSortingArrayWithMultipleItemsSortedExceptForOne(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithMultipleItemsInRandomOrder() {
+        SortingTests.testSortingArrayWithMultipleItemsInRandomOrder(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithMultipleItemsInCorrectOrder() {
+        SortingTests.testSortingArrayWithMultipleItemsInCorrectOrder(QuickSort.sort)
+    }
+    
+    func testSortingArrayWithMultipleItemsInReverseOrder() {
+        SortingTests.testSortingArrayWithMultipleItemsInReverseOrder(QuickSort.sort)
+    }
+    
+}
